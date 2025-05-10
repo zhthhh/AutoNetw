c45da969d88eea53f0334cf2936db3f2f6b0fe54
zxg <3152430961@qq.com>
2025-05-10 23:45:05 +0800

    V1.1.1

--- autoNetwork/getIP.ps1
+++ autoNetwork/getIP.ps1
@@ -2,7 +2,7 @@ $PSDefaultParameterValues['Out-Default:NoEcho'] = $true
 
 $scriptDirectory = $PSScriptRoot
 
-$relativeFilePath = Join-Path -Path $scriptDirectory -ChildPath "./campusIP.txt"
+$relativeFilePath = Join-Path -Path $scriptDirectory -ChildPath "./data/campusIP.txt"
 
 $ipAddresses = Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.IPAddress -match "^10.36" } | Select-Object -ExpandProperty IPAddress
 
